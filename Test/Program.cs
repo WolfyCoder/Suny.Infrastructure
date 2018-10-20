fba2e7b94cb52d2605bbe2c0d886fe45d7dc30af
sunwy <sunwy@samsundot.com>
2018-10-20 14:15:34 +0800

    mod program Name and publish nuget package

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,5 @@
-﻿using Suny.Infrastructure.Uitls;
-using System;
+﻿//using Suny.Infrastructure.Uitls;
+//using System;
 
 namespace Test
 {
@@ -7,17 +7,17 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            string strMd5 = MD5Util.Md5FromString("1234");
-            Console.WriteLine("string md5:" + strMd5);
-            string fileMd5 = MD5Util.Md5FromFile("user.gif");
-            Console.WriteLine(fileMd5);
-            Console.WriteLine(fileMd5== "0796582A8B780958C53B88AE247D9B37".ToLower());
-            var now = DateTime.Now;
-            Console.WriteLine(now);
-            var ts = now.ToUnixTimeMilliseconds();
-            Console.WriteLine(ts);
-            Console.WriteLine(ts.ToLocalDateTimeTime());
-            Console.Read();
+            //            string strMd5 = MD5Util.Md5FromString("1234");
+            //            Console.WriteLine("string md5:" + strMd5);
+            //            string fileMd5 = MD5Util.Md5FromFile("user.gif");
+            //            Console.WriteLine(fileMd5);
+            //            Console.WriteLine(fileMd5== "0796582A8B780958C53B88AE247D9B37".ToLower());
+            //            var now = DateTime.Now;
+            //            Console.WriteLine(now);
+            //            var ts = now.ToUnixTimeMilliseconds();
+            //            Console.WriteLine(ts);
+            //            Console.WriteLine(ts.ToLocalDateTimeTime());
+            //            Console.Read();
         }
     }
 }
